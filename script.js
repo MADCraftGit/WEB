0944cc50864bb65bfff39659e1201f3e3ea05db6
MADCraftGit <info.madcraft.server@gmail.com>
2025-09-02 08:44:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ document.querySelectorAll('.navbar a').forEach(link => {
     }, 225);
   });
 });
+
+document.querySelectorAll('.link a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    this.style.color = 'rebeccapurple';
+    
+    setTimeout(() => {
+      window.location.href = this.href;
+    }, 225);
+  });
+});
