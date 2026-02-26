0a523aaa400f0ee33a0f4b2c46a3089085284a8c
Macarat2009 <info.madcraft.server@gmail.com>
2026-02-26 18:56:32 +0100

    delay remove

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 console.log('Script loaded');
 
 
-document.querySelectorAll('.navbar a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    
-    this.style.color = '#3ba635';
-    
-    setTimeout(() => {
-      window.location.href = this.href;
-    }, 225);
-  });
-});
-
 window.addEventListener("scroll", function() {
   let arrow = document.querySelector(".top-arrow");
   
